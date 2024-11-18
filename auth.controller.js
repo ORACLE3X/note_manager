14127bdef13cd32f3eb869d26b13a73d8b621135
ORACLE3X <legendaryoracle8@gmail.com>
2024-11-18 17:01:56 +0100

    worked on advanced security in the server

--- auth.controller.js
+++ auth.controller.js
@@ -1,16 +1,28 @@
 const { compareSync } = require("bcryptjs");
 const AccountModel = require("./Account.models");
 const { APIError } = require("./apiError");
-
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv").config();
 exports.login= async (req, res, next )=>{
     try {
         const {email, password}= req.body;
         if(!email) return next(APIError.badRequest("Email is required"));
         if(!password) return next (APIError.badRequest("Password is required"));
         const userExist = await AccountModel.findOne({email});
+        // console.log(userExist);
         if(!userExist) return next (APIError.notFound("Account does not exist"));
         if(!compareSync(password,userExist.password)) return next (APIError.badRequest("Incorrect password"));
-        res.status(201).json({message : " Login sucessful"});
+        const accessToken = jwt.sign({id:userExist._id, email:userExist.email}, process.env.ACESS_TOKEN_SECRET,{expiresIn:"30s"});
+        // console.log(acessToken);
+        const refreshToken = jwt.sign({id:userExist._id, email:userExist.email}, process.env.REFRESH_TOKEN_SECRET,{expiresIn: "5m"});
+        userExist.refreshToken = refreshToken;
+        userExist.save();
+        res.clearCookie("note_app")
+        res.cookie("note_app", accessToken, {
+            httpOnly : false,
+            sameSite: "none",
+        })
+        res.status(200).json({messge: "Login sucessfull",accessToken, refreshToken})
     } catch (error) {
         next(error);
     }
