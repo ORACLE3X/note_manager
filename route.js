50ce55ba2ff28552ac94b4131cab6d15250203e5
ORACLE3X <legendaryoracle8@gmail.com>
2024-11-23 16:56:52 +0100

    Worked on the README FILE

--- route.js
+++ route.js
@@ -1,5 +1,5 @@
 const { APIError } = require("./apiError");
-const { login, logout } = require("./auth.controller");
+const { login, logout, refreshToken } = require("./auth.controller");
 const { creatUser, getAccounts, updateAccount, getAccountByID, deleteAccount, createNote, getNote, getNoteByTitle,  } = require("./handlers");
 const {  userRequired } = require("./middleware/auth.middleware");
 const router= require("express").Router();
@@ -11,6 +11,6 @@ router.delete("/account_id", deleteAccount)
 //createNote route
 router.post("/Note",userRequired, createNote);
 router.get("/Note", getNote)
-router.get("/note_id", getNoteByTitle).post("/auth/login", login).post("/auth/logout", userRequired, logout)
+router.get("/note_id", getNoteByTitle).post("/auth/login", login).post("/auth/logout", userRequired, logout).post("/auth/refreshtoken", refreshToken)
 router.use("*", APIError.notFound) 
 module.exports = router;
